0472697b5df8d97039ba1fb8af257b122919ad53
NocDevX <xdev.noc@gmail.com>
2022-10-24 09:25:58 -0300

    24/10/2022   - Adicionado setup do docker

---
+++ scripts/docker.bash
@@ -0,0 +1,28 @@
+apt install gnome-terminal
+
+#Cleanup
+apt remove docker-desktop
+rm -r "$HOME"/.docker/desktop
+rm /usr/local/bin/com.docker.cli
+apt purge docker-desktop
+
+apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+mkdir ./tmp
+wget -O ./tmp/docker-desktop.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.0-amd64.deb
+apt install -y ./tmp/docker-desktop.deb
+rm -R -f ./tmp
