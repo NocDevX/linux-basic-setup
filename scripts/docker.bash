c5aea829dd155ae0918e76a77207c13a25ef28fc
NocDevX <xdev.noc@gmail.com>
2022-10-24 09:34:28 -0300

    24/10/2022   - Ajustes no docker.bash

--- scripts/docker.bash
+++ scripts/docker.bash
@@ -1,16 +1,15 @@
-apt install gnome-terminal
+apt install -y \
+    gnome-terminal \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
 #Cleanup
 apt remove docker-desktop
 rm -r "$HOME"/.docker/desktop
 rm /usr/local/bin/com.docker.cli
 apt purge docker-desktop
-
-apt install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -20,7 +19,11 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-compose-plugin
 
 mkdir ./tmp
 wget -O ./tmp/docker-desktop.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.0-amd64.deb
